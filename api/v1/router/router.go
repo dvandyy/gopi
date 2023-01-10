0d5f9ec0b2eb97ef76c73c96706a9c4ad0622fa5
Tomas Boda <tomas.boda@vibration.sk>
2023-01-10 17:49:29 +0100

    extract router

---
+++ api/v1/router/router.go
@@ -0,0 +1,30 @@
+package router
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/bodatomas/gopi/api/v1/handlers"
+	"github.com/gorilla/mux"
+)
+
+func InitRouter(l *log.Logger) *mux.Router {
+
+	router := mux.NewRouter()
+
+  // Get subrouter
+	getRouter := router.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", handlers.NewHello(l).GetHello)
+
+  // Post subrouter
+	// postRouter := router.Methods(http.MethodPost).Subrouter()
+
+  // Put subrouter
+	// putRouter := router.Methods(http.MethodPut).Subrouter()
+
+  // Delete subrouter
+	// deleteRouter := router.Methods(http.MethodDelete).Subrouter()
+
+  return router
+}
+
--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,14 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/bodatomas/gopi/api/v1/handlers"
-	"github.com/gorilla/mux"
+	"github.com/bodatomas/gopi/api/v1/router"
 )
 
 func main() {
-  // Main router Mux
-	router := mux.NewRouter()
   // Logger for one place logging
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+  // Main router Mux
+	router := router.InitRouter(l)
 
   // Server configuration
   // TODO: implement env variables
@@ -27,19 +26,6 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-
-  // Get subrouter
-	getRouter := router.Methods(http.MethodGet).Subrouter()
-	getRouter.HandleFunc("/", handlers.NewHello(l).GetHello)
-
-  // Post subrouter
-	// postRouter := router.Methods(http.MethodPost).Subrouter()
-
-  // Put subrouter
-	// putRouter := router.Methods(http.MethodPut).Subrouter()
-
-  // Delete subrouter
-	// deleteRouter := router.Methods(http.MethodDelete).Subrouter()
 
   // Concurent server run and listen
 	go func() {
